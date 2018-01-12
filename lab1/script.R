74a4c838f04468ddbe793405b09982b34d06095f
Oleksandr Zaytsev <olk.zaytsev@gmail.com>
2018-01-12 14:38:19 +0200

    Split the data randomly into training and test data set

--- lab1/script.R
+++ lab1/script.R
@@ -1,4 +1,19 @@
 require(GGally)
 
+# Reading the credit score data
 credit <- read.csv("data/credit.csv", sep = ";", dec = ",")
-ggpairs(data = credit)
+
+# 1. Import the data and make appropriate visualisation (pairs, ggpairs)
+pairs(credit)
+ggpairs(credit)
+
+# 2. Split the data randomly into training and test data set (sample).
+
+#Sample Indexes
+indexes = sample(1:nrow(credit), size=0.2*nrow(credit))
+
+# Split data
+test = credit[indexes,]
+dim(test)  # 100 8
+train = credit[-indexes,]
+dim(train) # 400 11
