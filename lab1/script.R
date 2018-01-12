26a30e94fe024f41a8b0a96314aa609cee60517b
Oleksandr Zaytsev <olk.zaytsev@gmail.com>
2018-01-12 15:43:05 +0200

    Computed forecast errorrs

--- lab1/script.R
+++ lab1/script.R
@@ -2,6 +2,9 @@
 # relying on several explanatory variables.
 
 require(GGally)
+require(forecast)
+require(Metrics)
+require(margins)
 
 # Reading the credit score data
 credit <- read.csv("data/credit.csv", sep = ";", dec = ",")
@@ -33,3 +36,28 @@ confint(model)
 # 5. Compute the forecasts (point, prediction and confidence intervals)
 # for the test data set (predict, forecast).
 pred <- predict(model, newdata = credit.test, interval = "confidence")
+matplot(c(1:100), pred, type="p", pch=15, col=c(1,2,2), cex=0.8)
+pred <- predict(model, newdata = credit.test, interval = "prediction")
+forec <- forecast(model, newdata = credit.test)
+
+# 6. Compute forecast errors and analyse them.
+mse(credit.test$score, forec$mean)
+mae(credit.test$score, forec$mean)
+mape(credit.test$score, forec$mean)
+
+# 7. Make appropriate data transformation and repeat the above steps
+# for the new model.
+model <- lm(score ~ savings + I(income ^ 2) + log(savings + 1), credit.train)
+summary(model)
+confint(model)
+
+# 8. Compute the marginal effects for the original and the transformed
+# models (margins).
+model <- lm(score ~ savings + income, credit.train)
+margins(model)
+
+# 9. Compare the forecasting ability of the two models using loss functions
+# and using appropriate tests.
+
+# 10. The score variable is slightly skewed. Takings logs shall overcome
+# this problem. Check the forecasting ability of the log-score model.
